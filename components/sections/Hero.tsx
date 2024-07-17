60704f0607641137d05b99208e357cc636ac4b57
Lemirq <sharmavihaan190@gmail.com>
2024-07-16 23:43:52 -0400

    Added globe

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -3,11 +3,16 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import { AuroraBackground } from '../aurora';
 import SignUpWaitlist from '../SignUpWaitlist';
+import Cobe from '../cobe';
 const Hero = () => {
 	return (
 		<section id="hero" className="w-full h-full">
 			<AuroraBackground className="w-full hidden sm:fc sm:items-start relative z-10 min-h-screen pt-[25rem] md:pt-10 px-5 sm:px-10 pb-10">
 				<SignUpWaitlist />
+
+				<div className="absolute fr md:w-full top-24 md:top-[initial] md:left-[initial] md:-right-[40vw] lg:-right-[30vw] md:h-full size-72 z-10 left-5 overflow-hidden px-5">
+					<Cobe />
+				</div>
 			</AuroraBackground>
 			<AuroraBackground className="sm:hidden min-h-screen pt-48 pb-10">
 				<motion.div
