09b56019fa0d922146362127cf944a79aa924e86
Lemirq <sharmavihaan190@gmail.com>
2024-07-17 13:50:01 -0400

    Add Meet The Team section to home page and founders

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import Hero from '@/components/sections/Hero';
 import About from '@/components/sections/About';
+import MeetTheTeam from '@/components/sections/MeetTheTeam';
 
 export default function Home() {
 	return (
 		<main className="w-full overflow-x-hidden bg-dot-white/[0.2] bg-neutral-950 fc">
 			<Hero />
 			<About />
+			<MeetTheTeam />
 		</main>
 	);
 }
---
+++ data/people.jsx
@@ -0,0 +1,28 @@
+export const people = [
+	{
+		name: 'Vihaan Sharma',
+		title: 'Vihaan Sharma',
+		description: "Vihaan is a teen from Ontario passionate about Web Dev, AI, and ML. Fun Fact: I'm building this site right now!",
+		image: '/images/founders/vihaan.jpg',
+	},
+	{
+		name: 'Prabhdeep Singh',
+		title: 'Prabhdeep Singh',
+		description:
+			"Prabhdeep is a teen from Ontario passionate about state of the art deep learning research, and it's applications in STEM. Fun Fact: Besides programming I also enjoy space tech, and astronomy.",
+		image: '/images/founders/prabh.png',
+	},
+	{
+		name: 'Arjun Virk',
+		title: 'Arjun Virk',
+		description: "Hi! I'm working on the infrastructure!",
+		image: '/images/founders/arjun.jpg',
+	},
+	{
+		name: 'Rishi Shah',
+		title: 'Rishi Shah',
+		description:
+			'Rishi is a teenager from Ontario who is interested in medical AI and adversarial attacks. Fun Fact: Rishi enjoys 3D printing and tinkering with electronics in his free time.',
+		image: '/images/founders/rishi.jpg',
+	},
+];
